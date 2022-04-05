582fa9bddcd69a652f575c4d4c9abf2a45092e83
Igor Uriel Alves de Araujo <T3062455@ipea.gov.br>
2022-04-05 15:20:39 -0300

    é isso, vou me demitir hoje e viver de livestream

--- src/controller/livrosController.js
+++ src/controller/livrosController.js
@@ -6,6 +6,57 @@ class LivroController {
             res.status(200).json(livros);
         })
     }
-}
+
+    static listarLivroPorId = (req, res)=>{
+        const id = req.params.id;
+        livros.findById(id, (err, livros)=>{
+            if(err){
+                res.status(400).send({message: `${err.message} - Livro não encontrado`});
+            } else {
+                res.status(200).send(livros);
+            }
+        })
+    }
+
+    static cadastrarLivro = (req, res)=>{
+        let livro = new livros(req.body);
+        
+        livro.save((err)=>{
+            if(err){
+                res.status(500).send({message: `${err.message} - falha ao cadastrar o livro`});
+            } else {
+                res.status(201).send(livro.toJSON());
+            }
+        })
+    }
+
+    static atualizarLivro = (req, res)=>{
+        const id = req.params.id;
+
+        livros.findByIdAndUpdate(id, {$set: req.body}, (err)=>{
+            if(!err){
+                res.status(200).send({message: "Livro atualizado com sucesso"});
+            } else {
+                res.status(500).send({message: `${err.message} - O livro não foi atualizado`});
+            }
+        })
+    }
+
+    static deletarLivro = (req, res)=>{
+        const id = req.params.id;
+        livros.findByIdAndDelete(id, {$set: req.body}, (err)=>{
+            if(!err){
+                res.status(200).send({message: "Livro deletado com sucesso"});
+            } else {
+                res.status(500).send({message: `${err.message} - O livro não foi deletado`});
+            }
+        })
+    }
+
+
+    // static deletarLivro = (req, res)=>{
+
+    // }
+} 
 
 export default LivroController;
